c0fc1be838cbe946033bb134c8ed223dfce3e439
talkal123 <talkal153@gmail.com>
2024-12-05 09:55:37 +0200

    ss

--- vite-project/src/Pages/Create/Create.jsx
+++ vite-project/src/Pages/Create/Create.jsx
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRef } from 'react';
-import LeftSide from '../HomeFile/leftSide'; 
+import LeftSide from '../../Pages/HomeFile/leftSide';
 import '../Create/Create.css'
 
 
-function Create () {
+const Create = () => {
+
+  const inputRef = useRef(null)
+  const [image, setImage] = useState("")
+
+  const handleImageClick = () => {
+    inputRef.current.click();
+  }
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    setImage(event.target.files[0]);
+  }
+
   return (
-    <div>
-      <img src='./GoogleDocs.png' alt='' />
-      <input type='file' />
+    <div className="App">
+      <div className="leftSideHome">
+        <LeftSide />
+      </div>
+
+      <div className="middleSide">
+        <div className='middleSideCreate'>
+          {image ? <img style={{ width: "200px", height:"200px" }}  onClick={handleImageClick} src={URL.createObjectURL(image)} alt='' /> : <img style={{ width: "200px", height:"200px" }}  onClick={handleImageClick} src="/GoogleDocs.png" alt='' />}
+          <input
+            type='file'
+            ref={inputRef}
+            onChange={handleImageChange}
+            style={{ display: "none" }}></input>
+          <div className='ss'>
+            <input type='text' placeholder='Description'></input>
+          </div>
+          <button>Upload</button>
+        </div>
+
+      </div>
+
+
+
+
     </div>
+  );
+};
 
-  )
-}
+export default Create;
 
-export default Create
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Create () {
+//   const inputRef = useRef(null);
+
+
+//   const handleImageClick =
+//     inputRef.current.click();
+//   return (
+//     <>
+//     <div onClick={handleImageClick}>
+//       <img style={{width:"400px",height:"300px"}} src='./GoogleDocs.png' alt='' />
+//       <input type='file' ref={inputRef} style={{display:"none"}}/>
+//     </div>
+//     </>
+//   );
+// }
+
+// export default Create
